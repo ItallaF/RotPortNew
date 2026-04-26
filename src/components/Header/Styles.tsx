4a05c265c4fbe17e7c042d7b2affe5f4b1c957cf
Italla Felyne <itallafelynemf@gmail.com>
2026-04-26 15:16:30 -0300

    Fix - Header component reusable

--- src/components/Header/Styles.tsx
+++ src/components/Header/Styles.tsx
@@ -1,45 +1,42 @@
-import React from "react";
 import { StyleSheet } from "react-native";
+import { theme } from "../../theme";
 
 export const styles = StyleSheet.create({
   container: {
     justifyContent: "space-between",
-    height: 150,
-    padding: 10,
+    height: 80,
     overflow: "hidden"
   },
   overlay: {
-    ...StyleSheet.absoluteFillObject,
-    backgroundColor: "rgb(253, 253, 253)"
+    ...StyleSheet.absoluteFill,
+    backgroundColor: theme.colors.surface,
   },
   topBar: {
     flexDirection: "row",
     justifyContent: "space-between",
   },
   iconButton: {
-    borderRadius: 12,
     height: 40,
     width: 40,
-    marginTop: 50,
+    marginTop: 35,
     marginLeft: 20,
   },
   avatar: {
-    borderRadius: 50,
     height: 40,
     width: 40,
-    marginTop: 50,
+    marginTop: 35,
     marginRight: 20,
   },
   content: {
-    marginTop: 50,
+    marginTop: 35,
   },
   title: {
-    color: "#3fb9df",
-    fontSize: 22,
+    color: theme.colors.gradient[0],
+    fontSize: theme.typography.h2.fontSize,
     fontWeight: "700"
   },
   subtitle: {
-    color: "#3fb9df",
-    fontSize: 14
+    color: theme.colors.gradient[1],
+    fontSize: theme.typography.body.fontSize,
   }
 });
